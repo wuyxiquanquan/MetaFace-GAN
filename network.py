709d6c1f6385b6f8e0a9eb6abb873c936bc13456
wuyxiquanquan <elephantameler@cortexlabs.ai>
2018-10-03 10:40:51 +0800

    decoder

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import mxnet as mx
 
 
@@ -6,7 +7,7 @@ def conv(data, num_filter, kernel, name, stride=(1, 1), slope=0.1, use_px=False)
     num_filter = num_filter * 4 if use_px else num_filter
     conv = mx.sym.Convolution(data, kernel=kernel, pad=pad, stride=stride, num_filter=num_filter, name='conv' + name)
     IN = mx.sym.InstanceNorm(conv, name='conv_IN' + name)
-    out = mx.sym.LeakyReLU(IN, act_type='leaky', slope=0.1, name='act' + name)
+    out = mx.sym.LeakyReLU(IN, act_type='leaky', slope=slope, name='act' + name)
     if use_px:
         px1 = mx.sym.reshape(out, shape=(0, -4, 2, 2, -2), name='px1_' + name)  # (B, C, 2, 2, H, W)
         px2 = mx.sym.transpose(px1, axes=(0, 1, 4, 2, 5, 3), name='px2_' + name)  # (B, C, H, 2, W, 2)
@@ -21,28 +22,53 @@ def fc(data, num_hidden, name):
     return out
 
 
+def mapping(data, num_hidden, name):
+    fc1 = mx.sym.FullyConnected(data, num_hidden=num_hidden, name=name + '_fc1')
+    relu1 = mx.sym.Activation(fc1, act_type='relu', name='_relu1')
+    fc2 = mx.sym.FullyConnected(relu1, num_hidden=num_hidden, name=name + 'fc2')
+    out = mx.sym.Activation(fc2, act_type='relu', name='_relu2')
+    return out
+
+
 def QuanDecoder():
     # B, 512
     embedding = mx.sym.var('embedding_vector')
     # label [Hair, Age, Skin, Gender] B, 4
-    label = mx.sym.var('label')
+    label = mx.sym.var('decoder_label')
     # angle [yaw, roll] B, 2
     angle = mx.sym.var('angle')
-
+    # Mapping for yaw and roll
+    yaw_vector = mapping(embedding, 512, 'yaw_mapping')  # B, 512
+    roll_vector = mapping(embedding, 512, 'roll_mapping')
+    # 系数可考虑
+    pose_vector = mx.sym.broadcast_mul(angle[:, :1], yaw_vector) + mx.sym.broadcast_mul(angle[:, 1:], roll_vector)
+    vector = mx.sym.elemwise_add(embedding, pose_vector, name='vector_add')
+    vector = mx.sym.reshape(vector, shape=(0, 0, 1, 1), name='vector_reshape')  # B, 512, 1, 1
     # 1. Deconv (B, 512, 7, 7)
-    deconv1 = mx.sym.Deconvolution(embedding, kernel=(7, 7), num_filter=512, no_bias=True, name='decoder_deconv1')
+    deconv1 = mx.sym.Deconvolution(vector, kernel=(7, 7), num_filter=512, no_bias=True, name='decoder_deconv1')
     deconv_IN1 = mx.sym.InstanceNorm(deconv1, name='decoder_deconv_IN1')
     deconv_act1 = mx.sym.LeakyReLU(deconv_IN1, act_type='leaky', slope=0.1, name='decoder_deconv_act1')
     # 2. PX (B, 256, 14, 14)
     px2 = conv(deconv_act1, 256, kernel=(3, 3), name='decoder_2', use_px=True)
     # 3. PX (B, 128, 28, 28)
     px3 = conv(px2, 128, kernel=(3, 3), name='decoder_3', use_px=True)
+    # insert labels
+    label = mx.sym.reshape(label, shape=(0, 0, 1, 1), name='label_reshape')
+    label = mx.sym.broadcast_axis(data=label, axis=(2, 3), size=(28, 28), name='label_broadcast')  # B, 4, 28, 28
+    px3 = mx.sym.concat(px3, label, dim=1)  # B, 128+4, 28, 28
     # 4. PX (B, 64, 56, 56)
     px4 = conv(px3, 64, kernel=(3, 3), name='decoder_4', use_px=True)
-    # ------------------
-    # insert
-    # ------------------
     # 5. PX (B, 32, 112, 112)
     px5 = conv(px4, 32, kernel=(3, 3), name='decoder_5', slope=0.2, use_px=True)
-    # 6. Conv(B, 32, 112, 112) 5x5
-    conv6 = conv(px5, 32, kernel=(5, 5), name='decoder_6', slope=0.2, use_px=False)
+    # 6. Conv(B, 7, 112, 112) 5x5
+    conv6 = conv(px5, 4, kernel=(5, 5), name='decoder_6', slope=0.2, use_px=False)
+    # all label mask (B, 1, 112, 112)
+    mask = conv6[:, 0:1] + conv6[:, 1:2] + conv6[:, 2:3] + conv6[3:4]
+    out = mx.sym.broadcast_mul(conv6[4:], mask, name='attention')  # B, 3, 112, 112
+
+    return out
+
+
+def QuanDiscr():
+    # B, 3, 112, 112
+    fake_image = mx.sym.var('fake_image')
